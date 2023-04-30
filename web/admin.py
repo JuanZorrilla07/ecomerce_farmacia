69000c2f5c4bf5c3b9f91bd70cbcba01bd4be373
JuanZorrilla07 <juan.-andres@live.com>
2023-04-30 01:55:22 -0200

    se agrego todas las plantillas. para la pagina aun quedan xosas de retocar de las plantillas, pero lo principal esta. despues se trabajo en el clase producto. que la escribii mal y no queria cambiarlo lo cambiara mas tarde.

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categoria, Producuto
+
+admin.site.register(Categoria)
+#admin.site.register(Producuto)
+
+@admin.register(Producuto)
+class ProducutoAdmin(admin.ModelAdmin):
+    list_display = ('nombre','precio','Categoria')
+    list_editable = ('precio',)
